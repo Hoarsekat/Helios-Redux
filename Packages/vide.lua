0fa0278179747a0b5b5bbf610332860d664de197
Hoarsekat <hoarsekat@outlook.com>
2024-11-06 15:18:08 +1100

    feat: Setup wally dependencies

--- Packages/vide.lua
+++ Packages/vide.lua
@@ -1,3 +1,1 @@
-local REQUIRED_MODULE = require(script.Parent._Index["centau_vide@0.2.0"]["vide"])
-export type Source<T> = REQUIRED_MODULE.Source<T>
-return REQUIRED_MODULE
+return require(script.Parent._Index["centau_vide@0.2.0"]["vide"])
