c3a2936316f9781ef78bc913e2f7ecfcc9735562
Hoarsekat <hoarsekat@outlook.com>
2024-11-13 20:14:07 +1100

    Port over to Indium by Hoarsekat

--- Packages/classpp.lua
+++ Packages/classpp.lua
@@ -1,4 +1,1 @@
-local REQUIRED_MODULE = require(script.Parent._Index["tenebrisnoctua_classpp@1.2.0"]["classpp"])
-export type classData = REQUIRED_MODULE.classData 
-export type class<AccessSpecifier > = REQUIRED_MODULE.class<AccessSpecifier >
-return REQUIRED_MODULE
+return require(script.Parent._Index["tenebrisnoctua_classpp@1.2.0"]["classpp"])
---
+++ Packages/indium.lua
@@ -0,0 +1,1 @@
+return require(script.Parent._Index["hoarsekat_indium@0.1.0"]["indium"])
